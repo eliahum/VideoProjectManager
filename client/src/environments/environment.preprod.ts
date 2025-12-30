0fa5fc728ec2827aee22457095b5f6f6f50fb872
eliahum <eliahu.malkin@gmail.com>
2025-12-31 00:59:35 +0200

    fixes

--- client/src/environments/environment.preprod.ts
+++ client/src/environments/environment.preprod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_BASE_URL: 'https://video-project-manager-gstb.vercel.app' // Production API URL
+  API_BASE_URL: 'https://video-project-manager-ppr-server.vercel.app' // Production API URL
 };
